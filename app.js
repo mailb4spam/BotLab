4cdf8d88424300f5aa02bccce0b290a39fcd1781
mailb4spam <mailb4spam@gmail.com>
2018-09-28 12:23:56 +0300

    add task 4

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 var restify = require('restify');
 var builder = require('botbuilder');
+var i18n = require('i18n');
+var parser = require('./parse-inquire');
 
-// Setup Restify Server поднимаем http 
+const inMemoryStorage = new builder.MemoryBotStorage();
+
+// i18n configuration
+i18n.configure({
+    defaultLocale: process.env.DEFAULT_LOCALE ? process.env.DEFAULT_LOCALE : 'en',
+    directory: __dirname + '/locales'
+});
+
+// Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3982, function () {
    console.log('%s listening to %s', server.name, server.url); 
@@ -18,5 +28,11 @@ server.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
-});
+    session.userData.profile = parser.ParseInquire(session.message.text);
+    if (session.userData.profile.greeting) {
+        session.send(i18n.__("greeting"));
+    }
+    else {
+        session.send("You said: %s", session.message.text);
+    }
+}).set('storage', inMemoryStorage);
